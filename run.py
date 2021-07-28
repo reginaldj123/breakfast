db7baffd6e5cf759ce975c3afc479347745d817b
reginaldj123 <gunt7701@gmail.com>
2021-07-28 21:25:02 +0000

    http.server

--- run.py
+++ run.py
@@ -1,1 +1,20 @@
-# python code goes here
+import gspread 
+from google.oauth2.service_account import credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+    
+CREDS = credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+sales = SHEET.worksheet('sales')
+
+data = sales.get_all_values()
+
+print(data)
